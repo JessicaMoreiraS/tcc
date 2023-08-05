dd834994d8f48067c6ef91f65babc48d5ad2b61b
JessicaMoreiraS <jessica.moreira.roso@gmail.com>
2023-08-05 17:08:14 -0300

    first commit

---
+++ direcionamentoLogin.php
@@ -0,0 +1,15 @@
+<?php
+$sou = filter_input(INPUT_POST, 'sou');
+// $dup = var_dump($sou);
+// echo $dup;
+
+if($sou == "professor"){
+//buscar no db professor
+}else{
+    if($sou == "aluno"){
+    //buscar no db aluno
+    }else{
+        //error
+    }
+}
+?>
