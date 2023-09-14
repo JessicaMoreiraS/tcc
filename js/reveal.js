aadebf2ea1499f482c46866034c797aaafd5bce6
Thalita <119790076+FernandesThalita@users.noreply.github.com>
2023-09-14 09:19:39 -0300

    Add files via upload

---
+++ js/reveal.js
@@ -0,0 +1,109 @@
+//configuração padrao que os docs pedem 
+window.sr = ScrollReveal({reset: true});
+
+
+
+///
+//PAG de LOGIN
+
+    //- eclipses
+sr.reveal('.eclipses .img',{ 
+  duration: 1000,
+  origin: 'right',
+  distance: '30px',
+  delay: 1000
+});
+sr.reveal('.eclipse-1',{ 
+  duration: 1000,
+  origin: 'right',
+  distance: '30px',
+  delay: 500
+});
+sr.reveal('.eclipse-2',{ 
+  duration: 1000,
+  origin: 'right',
+  distance: '40px',
+  delay: 750
+});
+sr.reveal('.eclipse-3',{ 
+  duration: 1000,
+  origin: 'right',
+  distance: '50px',
+  delay: 950
+});
+/////////
+sr.reveal('.botoes_box',{ 
+  duration: 1000,
+  origin: 'top',
+  distance: '50px',
+});
+
+sr.reveal('.input',{ 
+  duration: 1000,
+  origin: 'right',
+  distance: '50px',
+});
+sr.reveal('#infosAcess a , .input_entrar',{ 
+  duration: 1000,
+  origin: 'right',
+  distance: '50px',
+});
+sr.reveal('.gestao_link',{ 
+  duration: 1000,
+  origin: 'left',
+  distance: '50px',
+});
+
+///////////////////////
+
+//PAG INDEX
+
+
+
+sr.reveal('.corpo-index .imagem-index',{ 
+  duration: 1000,
+  origin: 'left',
+  distance: '50px',
+});
+sr.reveal('.corpo-index .container-texto h2',{ 
+  duration: 1500,
+  origin: 'right',
+  distance: '50px',
+});
+sr.reveal('.corpo-index .container-texto p',{ 
+  duration: 1500,
+  origin: 'left',
+  distance: '70px',
+});
+
+sr.reveal('.corpo-index .container-texto button',{ 
+  duration: 1500,
+  origin: 'rigth',
+  distance: '70px',
+});
+///////////////////////
+
+//PAG Home GESTAO
+sr.reveal('#main_homeGestao .titulo',{ 
+  duration: 1000,
+  origin: 'right',
+  distance: '50px',
+});
+sr.reveal('#main_homeGestao .section_cards ',{ 
+  duration: 1000,
+  origin: 'top',
+  distance: '50px',
+});
+sr.reveal('#main_homeGestao .section_cards .card .img img ',{ 
+  duration: 1000,
+  origin: 'top',
+  distance: '50px',
+  delay: 300
+});
+sr.reveal('#main_homeGestao .section_cards .card .btn',{ 
+  duration: 1000,
+  origin: 'top',
+  distance: '50px',
+  delay: 300
+});
+///////////////////////
