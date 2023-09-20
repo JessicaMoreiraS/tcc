62f7821fe76e723576de473d2ac5f0765f2de5cb
FernandesThalita <thalitahfernades@gmail.com>
2023-09-20 11:10:11 -0300

    adicionando o sistema de update

---
+++ update.php
@@ -0,0 +1,119 @@
+<?php
+//iniciando sessão
+session_start();
+//verificação e confirmação que o usuario e´ gestor
+if ($_SESSION['idAcesso'] != 'gestaoSenai') {
+    header('Location: index.html');
+    exit; 
+}
+
+//incluindo conexao cm o banc
+include('conexao.php');
+
+
+if (isset($_GET['option']) && isset($_GET['id_atualizacao'])) {
+    $tabelaBuscar = $_GET['option'];
+    $id_atualizacao = $_GET['id_atualizacao'];
+
+    // sql que mostra todas as colunas da tabela em questao
+    $sqlBuscarTodasColunas = "SHOW COLUMNS FROM $tabelaBuscar";
+
+    //execuntando o sql e colocando este sql dentro de uma variavel
+    $resultado = $mysqli->query($sqlBuscarTodasColunas);
+
+    //aplicando campos como um array vazio (pq vai ser utilizado futuramente com uma função)
+    $campos = array();
+
+
+
+    while ($row = mysqli_fetch_assoc($resultado)) {
+
+        //construindo a array campos, com os valores da funcao fetch
+        $campos[] = $row['Field'];
+    }
+    //mysqli_fetch_assoc busca o resultado de uma consulta sql em linha, como array, entao o resultado de $sqlBuscarTodasColunas, é retornado nest função e aplicado na variavel $row
+
+
+
+
+
+    //variaveis para não aparecerem no form de atualizacao
+    // $ExcluirDaTabela1 = 'senha';
+    // $ExcluirDaTabela2 = 'redefinir_senha';
+    // $ExcluirDaTabela3 = 'id';
+
+    //substituindo os valores de campos(campos dos formuarios) por uma função que ira excluir s variavies criadas acima, da busca sql
+    // $campos = array_diff($campos, array($colunaExcluir1, $colunaExcluir2, $colunaExcluir3));
+
+
+    //verifica se o formuario é do metodo post
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+
+    
+        //sql da atualização de dados
+        //tabelaBuscar = tabela em questao
+        $sql = "UPDATE $tabelaBuscar SET ";
+
+        //for que itera(percorre as linhas), na array campos (todos os nomes das tabelas que é possivel atualizar)
+        foreach ($campos as $campo) {
+
+            //array post (do formulario), esta sendo recebida por uma variavel
+            $valor = $_POST[$campo];
+
+            //concatenar o campo (nome da tabela), com o valor atualizado, puxado do POST
+            $sql .= "$campo = '$valor', ";
+        }
+
+
+        //finaizaçao da consulta (informa o id)
+        //rtrim($sql, ', ') é uma funçaõ de remocao de caracteres, onde aqui esta removendo as virgulas na consulta (aplicadas anteriormente)
+        $sql = rtrim($sql, ', ') . " WHERE id = $id_atualizacao";
+
+        // verifica se foi executado a consulta e redireciona
+        if ($mysqli->query($sql)) {
+            header("Location: visualizar.php?view=$tabelaBuscar");
+        } 
+    }
+
+
+    //buscando as informaçoes das colunas do id referido
+    $sql = "SELECT * FROM $tabelaBuscar WHERE id = $id_atualizacao";
+    //aplicando a execuçao do sql acima, em uma variavel
+    $resultado = $mysqli->query($sql);
+
+    //buscando a linha do objeto
+    //nome da coluna = chave , valor da coluna = dados
+    $dados = mysqli_fetch_assoc($resultado);
+
+    
+    ?>
+    <!DOCTYPE html>
+    <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>update</title>
+    </head>
+    <body>
+        <form method="POST" action="<?php echo "update.php?option=$tabelaBuscar&id_atualizacao=$id_atualizacao" ?>">
+            <?php
+
+            //iteraçao pela array campos (array do fetch que armazena os dados da tabela)
+            foreach ($campos as $campo) {
+                // valor atual = dados do campo em especifico
+                $valorAtual = $dados[$campo];
+                ?> 
+                <label><?php echo $campo; ?></label>
+                <input type="text" id="<?php echo $campo; ?>" name="<?php echo $campo; ?>" value="<?php echo $valorAtual; ?>"><br>
+                <?php
+            }
+            ?>
+            <input type="submit" value="atualizar">
+        </form>
+    </body>
+    </html>
+    <?php
+} 
+
+
--- visualizar.php
+++ visualizar.php
@@ -1,55 +1,71 @@
 <?php
 session_start();
-if($_SESSION['idAcesso'] != 'gestaoSenai'){
+if ($_SESSION['idAcesso'] != 'gestaoSenai') {
     header('Location: index.html');
+    exit;
 }
-if(filter_input(INPUT_GET, 'view')){
-    if($_GET['view'] == 'professor'){
+if (filter_input(INPUT_GET, 'view')) {
+    if ($_GET['view'] == 'professor') {
         $tabelaBusca = "professor";
         $campos = ['cpf', 'nome', 'email'];
-    }else if($_GET['view'] == 'maquina'){
-        $tabelaBusca = "maquina";
-        $campos = ['id','modelo','fabricante'];
-    } else if($_GET['view'] == 'professor'){
+    } else if ($_GET['view'] == 'maquina') {
         $tabelaBusca = "maquina";
         $campos = ['id', 'modelo', 'fabricante'];
-    } else if($_GET['view'] == 'aluno'){
+    } else if ($_GET['view'] == 'professor') {
+        $tabelaBusca = "maquina";
+        $campos = ['id', 'modelo', 'fabricante'];
+    } else if ($_GET['view'] == 'aluno') {
         $tabelaBusca = "aluno";
         $campos = ['nome', 'email'];
     }
 }
 
-function trMaisTbody($campos, $tabContent, $tabelaBuscar){
+function trMaisTbody($campos, $tabContent, $tabelaBuscar)
+{
     include('conexao.php');
     //cria a tr com os campos segundo o array de campos?>
     <tr>
         <?php
-            for($i=0; $i< count($campos); $i++){?>
-            <th><?php $campos[$i]?></th>
-        <?php
-        }?>
+        for ($i = 0; $i < count($campos); $i++) { ?>
+            <th>
+                <?php $campos[$i] ?>
+            </th>
+            <?php
+        } ?>
     </tr>
-    
+
     <tbody>
         <?php
-        $conteudo = $mysqli -> query($tabContent);
-        while ($row = mysqli_fetch_assoc($conteudo)){?>
-        <tr>
-            <?php
-            for($i=0; $i<count($campos); $i++){?>
-                <td><?php echo $row[$campos[$i]]?></td>
-            <?php
-            $id_para_deletar = $row['id'];
-            }?>
-            <!-- td a href="delete.php?option=aluno/id_delecao=$row['id']" button -->
-            <td>
-                <a href="<?php echo "delete.php?option=$tabelaBuscar&id_delecao=$id_para_deletar" ?>">
-                delete
-                </a>
-            </td>
+        $conteudo = $mysqli->query($tabContent);
+        while ($row = mysqli_fetch_assoc($conteudo)) { ?>
+            <tr>
+                <?php
+                for ($i = 0; $i < count($campos); $i++) { ?>
+                    <td>
+                        <?php echo $row[$campos[$i]] ?>
+                    </td>
+                    <?php
+                    $id_para_modificar = $row['id'];
+                } ?>
+                <!-- td a href="delete.php?option=aluno/id_delecao=$row['id']" button -->
+                <td>
+                    <a href="<?php echo "delete.php?option=$tabelaBuscar&id_delecao=$id_para_modificar" ?>">
+                        delete
+                    </a>
+                </td>
+                <?php
+                if ($_GET['view'] == 'maquina' || $_GET['view'] == 'tipo') {
+                    echo '<td>
+                        <a href="update.php?option=' . $tabelaBuscar . '&id_atualizacao=' . $id_para_modificar . '">
+                         Atualizar
+                        </a>
+                         </td>';
+                }
+                ?>
+
             </tr>
             <?php
-        }?>
+        } ?>
     </tbody>
     <?php
 }
@@ -57,48 +73,53 @@ function trMaisTbody($campos, $tabContent, $tabelaBuscar){
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Visualizar</title>
 </head>
-<body>
-        <?php
-            include('conexao.php');
-            if(in_array("modelo", $campos)){
-                $busca = "SELECT * FROM maquina INNER JOIN tipo_maquina ON tipo_maquina.id = maquina.id_tipo_maquina";
-                //$buscar = "SELECT * FROM maquina";
-                $conteudo = $mysqli -> query($busca); 
-                while ($rowMaquina = mysqli_fetch_assoc($conteudo)){
-                
 
-                ?>
-                    <table>
-                        <tr>
-                            <th><?php $rowMaquina['id_tipo_maquina']; ?></th>
-                        </tr>
-                        <?php 
-                        $idTipo = $rowMaquina['id_tipo_maquina'];
-                        $tabContent = "SELECT * FROM $tabelaBusca WHERE id_tipo_maquina = $idTipo";
-                        trMaisTbody($campos, $tabContent, $tabelaBusca) ?>
-                    </table>
-                    <?php
-                }
-            }else{
-                ?>
-                <table>
-                    <?php 
-                    $tabContent = "SELECT * FROM $tabelaBusca";
-                    trMaisTbody($campos, $tabContent, $tabelaBusca) ?>
-                </table>
+<body>
+    <?php
+    include('conexao.php');
+    if (in_array("modelo", $campos)) {
+        $busca = "SELECT * FROM maquina INNER JOIN tipo_maquina ON tipo_maquina.id = maquina.id_tipo_maquina";
+        //$buscar = "SELECT * FROM maquina";
+        $conteudo = $mysqli->query($busca);
+        while ($rowMaquina = mysqli_fetch_assoc($conteudo)) {
+
+
+            ?>
+            <table>
+                <tr>
+                    <th>
+                        <?php $rowMaquina['id_tipo_maquina']; ?>
+                    </th>
+                </tr>
                 <?php
-            }
+                $idTipo = $rowMaquina['id_tipo_maquina'];
+                $tabContent = "SELECT * FROM $tabelaBusca WHERE id_tipo_maquina = $idTipo";
+                trMaisTbody($campos, $tabContent, $tabelaBusca) ?>
+            </table>
+            <?php
+        }
+    } else {
         ?>
+        <table>
+            <?php
+            $tabContent = "SELECT * FROM $tabelaBusca";
+            trMaisTbody($campos, $tabContent, $tabelaBusca) ?>
+        </table>
+        <?php
+    }
+    ?>
 </body>
+
 </html>
 <?php
-    if(filter_input(INPUT_GET, 'e')){
-        $mensagem_erro = filter_input(INPUT_GET, 'e');
-        echo '<script>erroLogin('.$mensagem_erro.')</script>';
-    }
+if (filter_input(INPUT_GET, 'e')) {
+    $mensagem_erro = filter_input(INPUT_GET, 'e');
+    echo '<script>erroLogin(' . $mensagem_erro . ')</script>';
+}
 ?>
