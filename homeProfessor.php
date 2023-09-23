2de5fa9329faf68b1774a7ed1b70a20f5d6043fb
FernandesThalita <thalitahfernades@gmail.com>
2023-09-23 18:41:59 -0300

    adicionando o update do perfil do professor

--- homeProfessor.php
+++ homeProfessor.php
@@ -30,10 +30,11 @@ echo $professorNome;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Professor</title>
 </head>
 
 <body>
+    <a href="update.php">editar perfil</a>
 <div class="turmas">
     <?php
     $conteudo = $mysqli->query($sqlConteudoCard);
