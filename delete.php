30cbdd6d9bd7c5ea496c8d72e3e2f7dd33b13446
FernandesThalita <thalitahfernades@gmail.com>
2023-10-01 14:11:53 -0300

    construindo a gestao de turmas e alunos

--- delete.php
+++ delete.php
@@ -9,49 +9,49 @@ include('conexao.php');
 
 if (isset($_GET["option"])) {
     $option = $_GET["option"];
-    $acao = $_GET['acao'];
-    echo $option;
 
-    $listaOption = ["professor", "aluno","maquina","tipo"];
-    for ($i=0; $i<count($listaOption);$i++){
+
+    $listaOption = ["professor", "maquina", "tipo"];
+    for ($i = 0; $i < count($listaOption); $i++) {
         if ($option == $listaOption[$i]) {
             $opcao = $listaOption[$i];
 
-            $alunoId = $_GET["id_delecao"]; ; 
-            $sqlDelete = "DELETE FROM $opcao WHERE id = $alunoId";
-    
+            $id = $_GET["id_delecao"];
+            ;
+            $sqlDelete = "DELETE FROM $opcao WHERE id = $id";
+
             if ($mysqli->query($sqlDelete)) {
-                header('Location: visualizar.php?view='.$opcao);
+                header('Location: visualizar.php?view=' . $opcao);
             } else {
-                header('Location: visualizar.php?e=9&view='.$opcao);
+                header('Location: visualizar.php?e=9&view=' . $opcao);
             }
         }
-
-        if($acao = 'deletarAlunoDaSala'){
-            $alunoId = $_GET["id_delecao"]; ;
-            $sqlDelete = "DELETE  FROM lista_aluno_sala  WHERE id_aluno = $alunoId";
-
-            
-            if ($mysqli->query($sqlDelete)) {
-             echo 'aluno deletado da sala';
-            } else {
-                echo 'aluno não deletado da sala'->mysql_error;
-            }
-        }else if($acao = 'deletarAluno'){
-            $alunoId = $_GET["id_delecao"]; ;
-            $sqlDelete = "DELETE FROM aluno WHERE id = $alunoId";
-
-            if ($mysqli->query($sqlDelete)) {
-                echo 'aluno deletado do sistema';
-               } else {
-                   echo 'aluno não deletado do sistema'->mysql_error;
-               }
-        }
-
         $stmt->close();
         $mysqli->close();
     }
 }
+if ( isset($_GET['acao'])) {
+
+    $acao = $_GET['acao'];
+    if ($acao == 'deletarAlunoDaSala') {
+        $id = $_GET["id_delecao"];
+        ;
+        $sqlDelete == "DELETE  FROM lista_aluno_sala  WHERE id_aluno = $id";
+        if ($mysqli->query($sqlDelete)) {
+            echo 'aluno deletado da sala';
+        } else {
+            echo 'aluno não deletado da sala'->mysql_error;
+        }
+    } else if ($acao == 'deletarAluno') {
+        $id = $_GET["id_delecao"];
+        ;
+        $sqlDelete = "DELETE FROM aluno WHERE id = $id";
+
+        if ($mysqli->query($sqlDelete)) {
+            echo 'aluno deletado do sistema';
+        } else {
+            echo 'aluno não deletado do sistema'->mysql_error;
+        }
+    }
+}
 ?>
-
-
