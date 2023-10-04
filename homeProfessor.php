71bafa1774aa8e08d24f2a82a103165ad2452080
FernandesThalita <thalitahfernades@gmail.com>
2023-10-04 08:54:27 -0300

    add front no home prof

--- homeProfessor.php
+++ homeProfessor.php
@@ -1,7 +1,7 @@
 <?php
 //inicisnd sessão 
 session_start();
-$paginasPermitemAcesso "login.php"
+$paginasPermitemAcesso = "login.php";
 if (!strpos($_SERVER['HTTP_REFERER'], $paginasPermitemAcesso)) {
     header('Location: index.html');
 }
@@ -28,8 +28,6 @@ $sqlConteudoCard = "SELECT DISTINCT sala.*, sala.turma
 $sqlConteudoRadios = "SELECT * FROM tipo_maquina";
 
 
-echo $professorNome;
-
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -38,22 +36,82 @@ echo $professorNome;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="js/script.js"></script>
-    <title>Professor</title>
+    <script
+      src="https://code.jquery.com/jquery-3.7.0.min.js"
+      integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g="
+      crossorigin="anonymous"
+    ></script>
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/mediaQuery.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <title><?php echo $professorNome?>- Inicial</title>
 </head>
 
-<body>
+<body id="body_homeProfessor">
+
+<header class="topo-inicial">
+      <img
+        width="140"
+        class="logo-inicial"
+        src="img/logo-senai-branco.png"
+        alt=""
+      />
+
+      <div class="icons">
+      <?php 
+      echo '
+      <a href="update.php?option=professor&id_atualizacao=' . $idProfessor . '">
+         <i
+           class="fa fa-user-circle"
+           style="color: rgb(255, 255, 255); cursor: pointer"
+         ></i>
+      </a>';
+     
+     ?>
+        <input
+          type="checkbox"
+          role="button"
+          aria-label="Display the menu"
+          class="menu"
+        />
+      </div>
+</header>
+    <main id="main_homeProfessor">
+    <section>
+        <div class="bem-vindo">
+          <h2>Bem vindo(a),<b> <?php echo $professorNome ?></b>!</h2>
+        </div>
+        <div class="btnCriarSala">
+          <a onclick="exibirFormCriarSala()">
+            <i class="fa fa-plus"></i>
+            Criar Turma
+          </a>
+        </div>
+        <div class="pesquisa">
+          <input
+            style="font-size: 16px"
+            placeholder="Pesquise por uma turma"
+            class="pesquisar"
+            type="text"
+          />
+        </div>
+      </section>
 
     <div class="turmas">
         <?php
-        //button exibe o formulario criar sala
-        echo '<button onclick="exibirFormCriarSala()">Criar Turma</button>';
+        // //button exibe o formulario criar sala
+        // echo '<button onclick="exibirFormCriarSala()">Criar Turma</button>';
 
         //http que redireciona para o professor editar suas infos
-        echo '
-     <a href="update.php?option=professor&id_atualizacao=' . $idProfessor . '">
-      Atualizar perfil
-     </a>
-      ';
+    //     echo '
+    //  <a href="update.php?option=professor&id_atualizacao=' . $idProfessor . '">
+    //   Atualizar perfil
+    //  </a>
+    //   ';
         //while que imprime os card (logica do Home Aluno)
         $conteudo = $mysqli->query($sqlConteudoCard);
         while ($row = mysqli_fetch_assoc($conteudo)) { ?>
@@ -76,6 +134,8 @@ echo $professorNome;
             <?php
 
         } ?>
+
+</main>
 
         <!-- formulario para criar uma nova sala -->
         <form id="form_cria_sala" action="cadastrarTurma.php" method="POST" style="opacity:0;">
