9a82412cd3f3607d7a2a616b277757148a7864d6
JessicaMoreiraS <jessica.moreira.roso@gmail.com>
2023-10-17 11:13:31 -0300

    graficos com dados vindos do esp

--- Checklist.php
+++ Checklist.php
@@ -1,12 +1,138 @@
+<?php
+    session_start();
+    if ($_SESSION['tipo'] != 'aluno' && $_SESSION['tipo'] != 'professor' && $_SESSION['tipo'] != 'gestor') {
+        header('Location: index.html');
+	}
+    
+    include('conexao.php');
+    
+
+    $idDaMaquina;
+    if($_GET['id_maquina']){
+        $idDaMaquina = $_GET['id_maquina'];
+    }
+
+    function buscarInfosMaquina($idMaquina){
+        include('conexao.php');
+        
+        $sql = "SELECT * FROM esp32 INNER JOIN atributo_tipo ON esp32.id_atributos = atributo_tipo.id WHERE id_maquina = $idMaquina";
+        $result = mysqli_query($mysqli, $sql);
+        
+        
+        if (!$result) {
+            die("Erro na consulta: " . mysqli_error($mysqli));
+        }
+        
+        $data = [];
+        while ($row = mysqli_fetch_assoc($result)) {
+            $array = [$row['atributo'], $row['valor']];
+            $data[] = $array;
+            foreach($data as $info){
+               // echo $info[0]." - ". $info[1]."<br>";
+                if($info[0] == "temperatura" || $info[0] == "velocidade" || $info[0] == "vibracao"){
+                    // echo "console.log('".$info[0]."')";
+                    echo '<script>';
+                    echo 'graficoVelocimetro('.$info[1].', "'.$info[0].'");'; // Chama a função JavaScript
+                    echo '</script>';
+                }
+            }
+        }
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/radar.js"></script>
+    <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
     <title>Checklist Form</title>
+    <style>
+        .grafico {
+        width: 100%;
+        height: 200px;
+        }
+    </style>
 </head>
-<body>
-    <h1>Checklist Form</h1>
+<body id="visualizarMaquinas">
+
+    <header class="topo-index">
+    <a href="inicialAluno.html"><img width="140" src="img/logo-senai-branco.png" alt="" /></a>
+    </header>
+    <main id="main_visualizarMaquina">
+        <section class="informacoes_maquina">
+            
+            <div class="nome_maquina">
+                <h2>Torno Mecanico</h2>
+            </div>
+
+            <div class="container-infoMaq">
+
+            <div class="img">
+                <img src="img/torno.png" width="300" alt="">
+                <figcaption>Fabricante: Lorem ipsum dolor sit amet</figcaption><br>
+            </div>
+
+            <div class="subInfo">
+                <div class="id">
+                    <h3>ID:0000000</h3>
+                    </div>
+                
+                    <div class="btns">
+                    <div class="btn">
+                        <a href="">Manual</a>
+                    </div>
+                    <div class="btn">
+                        <a href="#checklist">Checklist</a>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
+        
+            <section class="graficos"> 
+
+            <div>
+                <div class="card-grafico">
+                <div id="graficos"></div> 
+                </div>
+            </div>
+
+            
+            </section>
+        </section>
+
+        
+        <section class="checklist" id="checklist">
+            <h2>Checklist</h2>
+            <div class="checkbox-wrapper-19">
+                <input id="check1" type="checkbox" name="item_1">
+                <label class="check-box" for="check1">Item</label>
+            </div>
+                <div class="checkbox-wrapper-19">
+                    <input id="check2" type="checkbox" name="item_2">
+                    <label class="check-box" for="check2">Item</label>
+            </div>
+            <div class="checkbox-wrapper-19">
+                <input id="check3" type="checkbox" name="item_3">
+                <label class="check-box" for="check3">Item</label>
+            </div>
+
+    </section>
+</main>
+
+<script src="js/graficos.js"></script>
+
+<?php buscarInfosMaquina($idDaMaquina) ?>
+
+
+
+    <!-- <h1>Checklist Form</h1>
     <form action="processarForm.php" method="post">
         <label for="item_1">Item 1:</label>
         <input type="checkbox" name="item_1" value="1">
@@ -30,6 +156,6 @@
         <input type="checkbox" name="item_10" value="10">
 
         <input type="submit" value="Enviar Checklist">
-    </form>
+    </form> -->
 </body>
 </html>
---
+++ js/graficos.js
@@ -0,0 +1,80 @@
+function graficoVelocimetro(valorGrafico, nomeDiv){
+    
+    var elementoPai = document.getElementById("graficos")
+    var novaDiv = document.createElement("div");
+    novaDiv.id = nomeDiv;
+    novaDiv.className = "grafico";
+    // console.log(nomeDiv);
+    elementoPai.appendChild(novaDiv);
+
+    am5.ready(function() {
+    
+        var root = am5.Root.new(nomeDiv);
+        
+        root.setThemes([
+            am5themes_Animated.new(root)
+        ]);
+        
+        var chart = root.container.children.push(
+            am5radar.RadarChart.new(root, {
+                panX: false,
+                panY: false,
+                startAngle: 180,
+                endAngle: 360
+            })
+        );
+        
+        var axisRenderer = am5radar.AxisRendererCircular.new(root, {
+            innerRadius: -10,
+            strokeOpacity: 1,
+            strokeWidth: 15,
+            strokeGradient: am5.LinearGradient.new(root, {
+                rotation: 0,
+                stops: [
+                    { color: am5.color(0x19d228) },
+                    { color: am5.color(0xf4fb16) },
+                    { color: am5.color(0xf6d32b) },
+                    { color: am5.color(0xfb7116) }
+                ]
+            })
+        });
+        
+        var xAxis = chart.xAxes.push(
+            am5xy.ValueAxis.new(root, {
+                maxDeviation: 0,
+                min: 0,
+                max: 100,
+                strictMinMax: true,
+                renderer: axisRenderer
+            })
+        );
+                
+        var axisDataItem = xAxis.makeDataItem({});
+        axisDataItem.set("value", 0);
+                
+        var bullet = axisDataItem.set("bullet", am5xy.AxisBullet.new(root, {
+            sprite: am5radar.ClockHand.new(root, {
+                radius: am5.percent(99)
+                })
+        }));
+                
+        xAxis.createAxisRange(axisDataItem);
+                
+        axisDataItem.get("grid").set("visible", false);
+                
+        chart.appear(1000, 100);
+        
+        console.log(valorGrafico)
+        setInterval(() => {
+            axisDataItem.animate({
+            key: 'value',
+            to: valorGrafico,
+            duration: 800,
+            easing: am5.ease.out(am5.ease.cubic)
+            });
+        }, 2000);
+    
+    }); // end am5.ready()
+    
+
+}
