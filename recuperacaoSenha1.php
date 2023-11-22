800f438c4bde3c695c88a42499e00b73989097b3
FernandesThalita <thalitahfernades@gmail.com>
2023-11-22 11:15:33 -0300

    adicionando animacao em recuperacao senha

--- recuperacaoSenha1.php
+++ recuperacaoSenha1.php
@@ -71,6 +71,7 @@ $mysqli->close();
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script src="https://unpkg.com/scrollreveal"></script>
     <link rel="icon" type="image/png" href="img/favicon/favicon-32x32.png"/>
 </head>
 <body id="body_recuperacaoSenha">
@@ -170,6 +171,7 @@ $mysqli->close();
         });
     </script>
 <script src="js/script.js"></script>
+<script src="js/reveal.js"></script>
 </body>
 </html>
 
