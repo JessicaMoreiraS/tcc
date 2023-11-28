5b9b2ec3dd0478647f6c957faefff759466e4b7a
FernandesThalita <thalitahfernades@gmail.com>
2023-11-28 14:29:32 -0300

    update front cadastrarTipoMaquina

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 async function caixaSenhaGestao() {
     const { value: password } = await Swal.fire({
         title: 'Preencha sua senha',
-        input: 'password', 
+        input: 'password',
         inputLabel: 'Senha',
         inputPlaceholder: 'Sua senha',
         inputAttributes: {
@@ -61,27 +61,27 @@ function erroLogin(n) {
 //SCRIPTS DO HOME PROFESSOR//
 
 //front-end do botao de criar turma
-if(window.location.href.indexOf("homeProfessor") != -1){
+if (window.location.href.indexOf("homeProfessor") != -1) {
     var from_criar_sala = document.getElementById("section_form");
     var mostrarBotao = document.getElementById("criar_turma");
     var ocultarBotao = document.getElementById("cancelar_button");
-    mostrarBotao.addEventListener("click", function() {
-        from_criar_sala.style.display = "block"; 
+    mostrarBotao.addEventListener("click", function () {
+        from_criar_sala.style.display = "block";
     });
-    ocultarBotao.addEventListener("click", function() {
-        from_criar_sala.style.display = "none"; 
+    ocultarBotao.addEventListener("click", function () {
+        from_criar_sala.style.display = "none";
     });
 }
 ////////
 
 //TRECHO DO CODIGO QUE LIBERA A CRIAÇÃO DE SALA, HOME PROFESSOR
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     // aplicando os inputs qye precisam ser preechidos em variaveis
     const checkboxes = document.querySelectorAll('input[type="checkbox"]');
     const cadastrarSalaButton = document.querySelector('#submitCadastrarSala');
 
     //  ouvinte de eventos de mudança a todos os checkboxes
-    checkboxes.forEach(function(checkbox) {
+    checkboxes.forEach(function (checkbox) {
         checkbox.addEventListener('change', verificarFormulario);
     });
 
@@ -104,17 +104,17 @@ document.addEventListener("DOMContentLoaded", function() {
 const edit_button = document.getElementById("imgEditIcon");
 const cancel_button = document.getElementById("botaoCancelar");
 function liberarEdicaoPerfil() {
-  const form_perfil = document.getElementById("form_perfil");
-  const inputs = form_perfil.querySelectorAll("input");
-  cancel_button.style.display = 'block';  
-  inputs.forEach(function (input) {
-    input.removeAttribute('readonly');
-    if(input.placeholder == 'Nome' || input.placeholder == 'Turma'){
-        input.focus();
-    }
-  });
+    const form_perfil = document.getElementById("form_perfil");
+    const inputs = form_perfil.querySelectorAll("input");
+    cancel_button.style.display = 'block';
+    inputs.forEach(function (input) {
+        input.removeAttribute('readonly');
+        if (input.placeholder == 'Nome' || input.placeholder == 'Turma') {
+            input.focus();
+        }
+    });
 }
-function cancelarEdicaoPerfil(){
+function cancelarEdicaoPerfil() {
     location.reload()
 }
 
@@ -125,19 +125,31 @@ var contadorAtributo = 0;
 var contadorPeca = 0;
 var contadorItem = 0;
 
-function criarInputs(tipo){
+function criarInputs(tipo) {
     var div = document.getElementById(`novo${tipo}`);
-    var conteudo="";
-    if(tipo=="Atributo" && contadorAtributo<5){
+    var conteudo = "";
+    if (tipo == "Atributo" && contadorAtributo < 5) {
         contadorAtributo++;
-        conteudo = `<div>
-                        <input type="text" name="atributo${contadorAtributo}" placeholder="Atributo">
-                        <input type="text" name="vReferencia${contadorAtributo}" placeholder="Valor de referência">
-                    </div>`
+        conteudo = `
+                <div class="containerNovoAtt">
+                    <div>
+                            <div class="input_modal">
+                                <input class="inputAtt" type="text" name="atributo${contadorAtributo}" placeholder="Atributo">
+                            </div>
+                            <div class="input_modal">
+                                <input class="inputVReferencia"  type="text" name="vReferencia${contadorAtributo}" placeholder="Valor de referência">
+                            </div>
+                    </div>
+                    
+                </div>  
+                    `
+
     }
-    if(tipo=="Peca" && contadorPeca<5){
+    if (tipo == "Peca" && contadorPeca < 5) {
         contadorPeca++;
-        conteudo = `<div>
+        conteudo = `    
+                <div class="containerNovaPeca">
+                    <div>
                         <div class="input_modal">
                             <input class="inputNomePeca" type="text" name="peca${contadorPeca}" placeholder="Nome da peça" >
                         </div> 
@@ -147,17 +159,25 @@ function criarInputs(tipo){
                         </div>
                           
                         <div class="input_modal">
-                            <input  class="inputTrocaPeca" type="number" name="tempoTroca${contadorPeca}" placeholder="Tempo de troca em horas" min="1">
-                        </div>    
-                    </div>`
+                                <input  class="inputTrocaPeca" type="number"  placeholder="Tempo de troca em Horas" name="tempoTroca${contadorPeca}" min="1">
+                        </div>
+                    </div>
+                </div>        
+                    `
     }
-    if(tipo=="Item" &&  contadorItem<5){
+    if (tipo == "Item" && contadorItem < 5) {
         contadorItem++;
-        conteudo = `<div>
-                        <input type="text" name="item${contadorItem}" placeholder="Item para checklist manual">
-                    </div>`
+        conteudo = `
+            <div  class="containerNovoItem">
+                <div>
+                    <div class="input_modal">
+                        <input type="text"  class="inputItem" name="item${contadorItem}" placeholder="Item">
+                    </div>
+                </div>
+            </div> 
+                    `
     }
 
-    div.innerHTML+=conteudo;
+    div.innerHTML += conteudo;
 }
 
