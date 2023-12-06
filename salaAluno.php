96dfd9b0b2c5437e00244715475b005798c2eb04
Jéssica Moreira <100448388+JessicaMoreiraS@users.noreply.github.com>
2023-12-06 09:44:02 -0300

    led no card maquina

--- salaAluno.php
+++ salaAluno.php
@@ -89,6 +89,27 @@ if ($stmt) {
             align-self: center;
             justify-self: start;
         }
+        .temaCardId{
+            display:flex;
+            justify-content: space-around;
+        }
+        .ledOff,
+        .ledVerde,
+        .ledVermelho{
+            display: flex;
+            align-self: center;
+            margin-left:12px;
+            width: 20px;
+            height:20px;
+            border-radius:100px;
+            background-color: grey;
+        }
+        .ledVerde{
+            background-color: rgb(98, 199, 98);
+        }
+        .ledVermelho{
+            background-color: rgb(226, 96, 96);
+        }
     </style>
 </head>
 
@@ -123,6 +144,9 @@ if ($stmt) {
                 <?php
                 $conteudo = $mysqli->query($sqlBuscaMaquinas);
                 while ($rowMaquina = mysqli_fetch_assoc($conteudo)) {
+
+                    $ledVerde = $rowMaquina['led_verde'];
+                    $ledVermelho = $rowMaquina['led_vermelho'];
                     //utilizando a funcao string base64 do php para coverter a imagem, e aplicar ela no html
 
                     $imagem= $rowMaquina['imagem'];
@@ -144,9 +168,18 @@ if ($stmt) {
                         <div class="content">
                             <h2 id="modelo-maquina">
                                 <?php echo $rowMaquina['modelo'] ?>
-                                <p id="ID-maquina">
-                                    <?php echo 'ID: ' . $rowMaquina['id'] ?>
-                                </p>
+                                <div class="temaCardId" id="ID-maquina">
+                                    <?php echo 'ID: ' . $rowMaquina['id'];?>
+                                    <?php 
+                                        if($ledVerde == "ON"){
+                                            echo '<div class="ledVerde"></div>';
+                                        }else if($ledVermelho == "ON"){
+                                            echo '<div class="ledVermelho"></div>';
+                                        }else{
+                                            echo '<div class="ledOff"></div>';
+                                        }
+                                    ?>
+                                </div>
 
                             </h2>
                             <p>
